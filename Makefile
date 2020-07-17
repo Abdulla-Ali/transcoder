ebadf78db92f4d69af0bc39b04e02bde6f11bd04
digehode <digehode@googlemail.com>
2020-07-17 13:53:57 +0100

    Basic layout of the project and documentation put together

---
+++ Makefile
@@ -0,0 +1,18 @@
+INVENV = $(shell pip3 -V | grep 'venv')
+current_dir = $(shell pwd)
+
+prereqs: venvcheck FORCE
+	pip install -r requirements.txt
+
+venv: FORCE
+	python3 -m venv venv
+
+venvcheck:
+ifeq ($(INVENV),)
+	$(error You should only run this from within the venv. Use '. ./venv/bin/activate')
+else
+	@echo "venv check passed\n"
+endif
+
+
+FORCE:
---
+++ src/transcoder.py
@@ -0,0 +1,25 @@
+
+
+
+def main():
+    """This function is called when this file is executed as a
+program. Usually we don't bother putting a docstring in this function,
+but I decided to add this one as a simple example. 
+
+Notice that the String is 'triple quoted'. Usually, a String in Python
+is bounded by one single or double quote mark. 'Like this' "or like
+this". You can also use triples like this one does. It has a few
+benefits that sometimes come in handy. In particular, you can easily
+include single and double quote characters within it, because it takes
+three in a row to signal the end. You can also go across multiple
+lines, like I have here.
+
+    """
+    print("Hello World")
+    
+
+
+
+
+if __name__=="__main__":
+    main()
